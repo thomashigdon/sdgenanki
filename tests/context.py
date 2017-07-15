9c172fbd64d7b87a6c689ed7e57f7041b3aebf82
Thomas Higdon <thomas.higdon@sonos.com>
2017-07-15 17:48:37 -0400

    Fleshed out git dependencies and tested imports.

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,12 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+gitdeps = [ "anki", "genanki" ]
+
+for dep in gitdeps:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', dep))
+    print(sys.path)
 
 import sdgenanki
