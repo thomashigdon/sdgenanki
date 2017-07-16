efef9cc6739dce9c6a883bdc8eae48f5fd533136
Thomas Higdon <thomas.higdon@sonos.com>
2017-07-15 20:41:05 -0400

    Add import test for bs4

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,10 +1,5 @@
 from context import sdgenanki
-import sys
-import os
 def test_import():
-    gitdeps = [ "anki", "genanki" ]
-    for dep in gitdeps:
-        sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', "deps", dep))
-    print(sys.path)
     import anki
     import genanki
+    import bs4
