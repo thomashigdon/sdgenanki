a77fc4ce721e9c4340e982f0a8ae367b5fea6296
Thomas Higdon <thomas.higdon@sonos.com>
2017-07-15 20:44:26 -0400

    Fix syspath stuff.

--- tests/context.py
+++ tests/context.py
@@ -4,9 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 gitdeps = [ "anki", "genanki" ]
-
 for dep in gitdeps:
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', dep))
-    print(sys.path)
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', "deps", dep))
 
 import sdgenanki
