134fb836035402d3743b7ecab737c6dc515e662f
Thomas Higdon <thomas.higdon@sonos.com>
2017-07-17 21:31:50 -0400

    Scraping conjugation now works.

--- sdgenanki/scraper.py
+++ sdgenanki/scraper.py
@@ -1,25 +1,46 @@
 import requests
-from bs4 import BeautifulSoup
-from bs4.element import NavigableString
+import re
+import json
 
 SD_ROOT = "http://www.spanishdict.com/"
+SD_API_ROOT = "http://translate1.spanishdict.com/api/v1/verb"
 
-def get_conj(verb):
-    url = SD_ROOT + "conjugate/" + verb
+def get_english_verb(text):
+    match = re.search(r"window.SD_QUICKDEF\s+=\s*'(.*)'", text)
+    if match:
+        return match.group(1)
+    raise Exception("blah")
+
+def get_conj_data(verb, lang):
+    url = SD_API_ROOT + "?q=%s&source=%s" % (verb, lang)
+    result = requests.get(url)
+    result = json.loads(result.text)
+    return result
+
+def get_conj(spanish_verb):
+    subjects = ("yo", "tú", "él/ella", "nosotros", "vosotros", "ellos")
+    url = SD_ROOT + "conjugate/" + spanish_verb
     r = requests.get(url)
     r.raise_for_status()
     page = r.text
-    soup = BeautifulSoup(page, "html.parser")
-    word_divs = soup.find_all("div", class_="vtable-word-contents")
-    words = {}
-    for word_div in word_divs:
-        form = word_div.parent.parent("td")[0].text
-        word_text = word_div.find("div", class_="vtable-word-text")
-        if not word_text: continue
-        word = "".join(word_text(string=True))
-        conjugation = word_text["data-tense"]
-        if not conjugation in words:
-            words[conjugation] = { form : word }
-        else:
-            words[conjugation][form] = word
+    english_verb = get_english_verb(page)
+    spanish_conj = get_conj_data(spanish_verb, "es")
+    english_conj = get_conj_data(english_verb, "en")
+    words = []
+    spanish_para = spanish_conj['data']['paradigms']
+    english_para = english_conj['data']['paradigms']
+    for form, vals in english_para.items():
+        if not vals or not spanish_para[form]: continue
+        translations = []
+        for s, e in zip(spanish_para[form], vals):
+            if not (s and e): continue
+            translations.append(
+                    (("spanish" , s['word']),
+                     ("english" , e['word']),
+                     ("tense" , form),
+                    # ("subject" , "yo"),
+                    ))
+        words.append(translations)
+
+
     return words
--- tests/test_conj.py
+++ tests/test_conj.py
@@ -1,25 +1,48 @@
 from context import sdgenanki
 import sdgenanki.scraper as scraper
+
+import os
+import pytest
 import requests_mock
 
 test_data = \
 {
     "tener" :
-    {
-        "presentIndicative" :
-        {
-            "yo" : "tengo"
-        }
-    }
+    [
+        (("spanish" , "tengo"),
+         ("english" , "I have"),
+         ("tense" , "presentIndicative"),
+#         ("subject" , "yo"),
+        ),
+    ]
 }
 
-def test_conj():
-    with requests_mock.mock() as m:
-        verb = "tener"
-        url = "http://www.spanishdict.com/conjugate/{}".format(verb)
-        with open("tests/tenertest.html") as f:
-            text = f.read()
-        m.get(url, text=text)
-        result = scraper.get_conj(verb)
-        yo = result["presentIndicative"]["yo"]
-        assert yo == test_data["tener"]["presentIndicative"]["yo"]
+def get_test_text(filename):
+    with open(os.path.join("tests", filename)) as f:
+        text = f.read()
+        return text
+
+@pytest.fixture
+def mock_request():
+    m = requests_mock.mock()
+    verb = "tener"
+
+    urls = [ ("http://www.spanishdict.com/conjugate/{}".format(verb),
+             "tenertest.html"),
+             ("http://translate1.spanishdict.com/api/v1/verb?q=tener&source=es",
+              "verb_english.json"),
+             ("http://translate1.spanishdict.com/api/v1/verb?q=to%20have&source=en",
+              "verb_english.json"),
+             ]
+    for url in urls:
+        text = get_test_text(url[1])
+        m.get(url[0], text=text)
+    return m
+
+def test_get_english_verb(mock_request):
+    text = get_test_text("tenertest.html")
+    assert scraper.get_english_verb(text) == "to have"
+
+def test_conj(mock_request):
+    result = scraper.get_conj("tener")
+    assert test_data["tener"][0] in [x[0] for x in result]
